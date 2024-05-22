c37bd871df06a16b346bb0c39de0c12926d7ff2a
unknown <105841107121@unismuh.ac.id>
2024-05-22 17:03:56 +0800

    lab1

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+const App = () => {
+  return (
+    <View style={{flex : 1, 
+    }}>
+      <View style={{ flex : 1, flexDirection:"row", backgroundColor: 'yellow'}}>
+        <View style={{ flex : 1, flexDirection: 1, backgroundColor: 'yellow'}}>
+        </View>
+        <View style={{ flex : 1, flexDirection: 1, backgroundColor: 'green'}}>
+        </View>
+      </View>
+      <View style={{ flex : 1, backgroundColor: 'red'}}>
+    </View>
+    <View style={{ flex : 1, backgroundColor: 'blue'}}>
+    </View>
+  </View>
+  )
+}
+export default App
