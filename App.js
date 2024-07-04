263a8354839d3b2e230e4c6be5dff2a30fef9e58
unknown <105841107121@unismuh.ac.id>
2024-07-04 16:58:19 +0800

    Navigation and Taskbar

---
+++ App.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { View, Text, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './Login'
+import Forgot from './Forgot'
+import SignUp from './SignUp'
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Ke Halaman Login" onPress={() => navigation.navigate('Login')}/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Forgot" component={Forgot} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
